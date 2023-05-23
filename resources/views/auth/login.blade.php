78dc47cd9eb006c427cf31d80d43549ee7d0dec0
Aung Min Oo <aungminoo1314@gmail.com>
2023-05-23 18:42:32 +0900

    login and register template design fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,70 +1,92 @@
-@extends('layouts.app')
+@extends('layouts.app_plain')
+@section('title','Login')
+
+@section('css')
+<style>
+
+</style>
+@endsection
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+    <div class="row justify-content-center align-content-center vh-100">
+        <div class="col-md-4">
+            <div class="card" style="height: 500px;">
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <!-- Pills navs -->
+                    <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                        <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="{{route('login')}}" role="tab"
+                            aria-controls="pills-login" aria-selected="true">Login</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="{{route('register')}}" role="tab"
+                            aria-controls="pills-register" aria-selected="false">Register</a>
+                        </li>
+                    </ul>
+                    <!-- Pills navs -->
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                    <!-- Pills content -->
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="text-center mb-3">
+                                    <p>Sign in with:</p>
+                                    <button type="button" class="btn-primary btn btn-link btn-floating mx-1">
+                                        <i class="fab fa-facebook-f text-white"></i>
+                                    </button>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <button type="button" class="btn-primary btn btn-link btn-floating mx-1">
+                                        <i class="fab fa-google text-white"></i>
+                                    </button>
+
+                                    <button type="button" class="btn-primary btn btn-link btn-floating mx-1">
+                                        <i class="fab fa-twitter text-white"></i>
+                                    </button>
+
+                                    <button type="button" class="btn-primary btn btn-link btn-floating mx-1">
+                                        <i class="fab fa-github text-white"></i>
+                                    </button>
+                                </div>
+
+                                <p class="text-center">or</p>
+
+                                <!-- Email input -->
+                                <div class="form-outline border-bottom">
+                                    <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" autocomplete="current-email"/>
+                                    <label class="form-label" for="email">Email</label>
+                                </div>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <!-- Password input -->
+                                <div class="form-outline mt-4 border-bottom">
+                                    <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name=password>
+                                    <label class="form-label" for="password">Password</label>
+                                </div>
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-primary btn-block my-4">Sign in</button>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                <!-- Register buttons -->
+                                <div class="text-center">
+                                    <p>Not a member? <a href="{{route('register')}}">Register</a></p>
                                 </div>
-                            </div>
+                            </form>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                    <!-- Pills content -->
                 </div>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,93 @@
-@extends('layouts.app')
+@extends('layouts.app_plain')
+@section('title','Register')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
+    <div class="row justify-content-center align-content-center vh-100">
+        <div class="col-md-4">
+            <div class="card" style="height: 500px;">
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    <!-- Pills navs -->
+                    <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                        <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="tab-login" data-mdb-toggle="pill" href="{{route('login')}}" role="tab"
+                            aria-controls="pills-login" aria-selected="true">Login</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="tab-register" data-mdb-toggle="pill" href="{{route('register')}}" role="tab"
+                            aria-controls="pills-register" aria-selected="false">Register</a>
+                        </li>
+                    </ul>
+                    <!-- Pills navs -->
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    <!-- Pills content -->
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <!-- Name input -->
+                                <div class="form-outline border-bottom">
+                                    <input type="name" id="name" class="form-control @error('name') is-invalid @enderror" name="name"/>
+                                    <label class="form-label" for="name">Username</label>
+                                </div>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <!-- Email input -->
+                                <div class="form-outline border-bottom mt-4">
+                                    <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" autocomplete="current-email"/>
+                                    <label class="form-label" for="email">Email</label>
+                                </div>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <!-- Password input -->
+                                <div class="form-outline mt-4 border-bottom">
+                                    <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name=password>
+                                    <label class="form-label" for="password">Password</label>
+                                </div>
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                                <!-- Password-confirm input -->
+                                <div class="form-outline mt-4 border-bottom">
+                                    <input type="password" id="password-confirm" class="form-control @error('password-confirm') is-invalid @enderror" name=password_confirmation>
+                                    <label class="form-label" for="password-confirm">password-confirm</label>
+                                </div>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+                                @error('password_confirmation')
+                                    <span role="alert">
+                                        <strong class="font-weight-bold text-danger">{{ $message }}</strong>
+                                    </span>
+                                @enderror
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                                <!-- Checkbox -->
+                                <div class="form-check d-flex justify-content-center my-4">
+                                    <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
+                                    aria-describedby="registerCheckHelpText" />
+                                    <label class="form-check-label" for="registerCheck">
+                                        I have read and agree to the terms
+                                    </label>
+                                </div>
+
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-primary btn-block my-4">Register</button>
+                            </form>
                         </div>
-                    </form>
+                    </div>
+                    <!-- Pills content -->
                 </div>
             </div>
         </div>
